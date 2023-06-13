022f9c9584b2f2328e3db41f51f0355369919e05
KayesKhanAkash <kayeskhanakash@gmail.com>
2023-06-14 01:31:07 +0600

    fix some functional issues

--- src/components/Loading/LoadingVideoCart.js
+++ src/components/Loading/LoadingVideoCart.js
@@ -8,9 +8,9 @@ const LoadingVideoCart = ({ className }) => {
         <div className="flex gap-3 mt-4 w-full">
           <div className="rounded-full min-h-[45px] min-w-[45px] max-h-[45px] max-w-[45px] bg-gray-900"></div>
           <div className="flex flex-col gap-2 w-full">
-            <div className="rounded-lg bg-gray-900 w-[90%] h-4"></div>
-            <div className="rounded-lg bg-gray-900 w-[30%] h-3"></div>
-            <div className="rounded-lg bg-gray-900 w-[45%] h-3"></div>
+            <div className="rounded-lg bg-gray-900 w-[200px] h-4"></div>
+            <div className="rounded-lg bg-gray-900 w-[100px] h-3"></div>
+            <div className="rounded-lg bg-gray-900 w-[130px] h-3"></div>
           </div>
         </div>
       </div>
--- src/components/VideoDetails/Description.js
+++ src/components/VideoDetails/Description.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { formatNumber, fromNow } from "../../Utils/functions";
 
 const Description = ({ videoDetails }) => {
+  const [seeMore, setSeeMore] = useState(false);
   return (
-    <div className="description mt-10 hover:bg-btn_hover overflow-auto p-4 rounded-xl text-sm bg-[#272727] text-white">
+    <div
+      className={`${
+        seeMore ? "overflow-auto" : "overflow-hidden max-h-[150px]"
+      } description cursor-pointer mt-10 hover:bg-btn_hover p-4 rounded-xl text-sm bg-[#272727] text-white`}
+      onClick={(e) => {
+        e.stopPropagation();
+
+        setSeeMore(true);
+      }}
+    >
       <p className="mb-4 font-semibold text-white text-base">
         <span>{formatNumber(videoDetails?.stats?.views)} views</span>{" "}
         <span>{fromNow(videoDetails?.publishedDate)}</span>
@@ -11,6 +21,18 @@ const Description = ({ videoDetails }) => {
       <pre className="font-['Roboto'_,sans-serif]">
         {videoDetails?.description}
       </pre>
+
+      <button
+        type="button"
+        className="text-lg text-blue-500 hover:underline"
+        onClick={(e) => {
+          e.stopPropagation();
+
+          setSeeMore(false);
+        }}
+      >
+        See less
+      </button>
     </div>
   );
 };
